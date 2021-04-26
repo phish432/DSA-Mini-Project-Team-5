b819ac150b4dca39b91755c5ba1f85e0852a4943
Akash C R <akash.c@research.iiit.ac.in>
2021-04-26 21:55:22 +0530

    sample code folder corrected

--- Sample Code/node.h
+++ Sample Code/node.h
@@ -1,16 +1,16 @@
 /*
-    The idea behind Tree Search Library is that, once given the 'comparator' function between nodes by
+    The idea behind Tree Search Library is that, once given the 'comparator' function between nodes by 
     the user, the library can use this to perform any kind of search by the change of just one function.
-
-    The comparator function takes as input the pointers of two nodes and outputs whether the first is
-    lesser than the other. You can use this comparator for deciding the order of 2 nodes in the priority
+    
+    The comparator function takes as input the pointers of two nodes and outputs whether the first is 
+    lesser than the other. You can use this comparator for deciding the order of 2 nodes in the priority 
     queue.
 
     This file will be what the user will edit, while trying to use your library. Once you complete writing
-    the library, you have to (as an user of the library), write the comparators for DFS, BFS, greedy
+    the library, you have to (as an user of the library), write the comparators for DFS, BFS, greedy 
     search (A-star lite).
 
-    Check out the input file ts_data_1.txt. It has input of format
+    Check out the input file ts_data_1.txt. It has input of format 
     <number_of_nodes>:n
     <state_number> <value> <parent_state_number>
     ...
@@ -20,28 +20,18 @@
     (in the struct) and try to write the priority queue using the comparator.
 */
 
-
-#ifndef _nodeheader
-#define _nodeheader
-
-
 #include <stdio.h>
-#include <stdbool.h>
 
-typedef struct node node;
-typedef node* ptrtonode;
-
-struct node
+typedef struct node
 {
     int state_number;
     int value;
     int parent;
-    node *ptrtoparent;
 
     // You (as a developer) can add any extra information here [Will not be touched by user]
     int depth, seen_time, number_of_children;
     node *children[10000];
-};
+} node;
 
 bool node_comparator(node *a, node *b)
 {
@@ -54,8 +44,3 @@ void input_node(node *a)
     // inputs the information of one node. Use this in a loop to input all the nodes.
     scanf("%d %d %d", &(a->state_number), &(a->value), &(a->parent));
 }
-
-
-//void make_tree(node *a[], int size)
-
-#endif
