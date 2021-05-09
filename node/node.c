2e356b40c05f3865b2d8fce10b5eff8be4e00724
Agrim Rawat <rawat.agrim432@gmail.com>
2021-05-09 12:19:16 +0530

    Updated and commented files (by Atharva due to technical diffuculty)

--- node/node.c
+++ node/node.c
@@ -1,28 +1,53 @@
 #include "node.h"
 
-bool node_comparator(node *a, node *b) {
-    node x = *a;
-    node y = *b;
+/*	Comparator:
+	The comparator picks the suitable comparison parameter based on the global flag variable.
+	Returns true if the 1st node has a higher priority than the 2nd node
+	For DFS, we pick higher seen time
+	For BFS, we pick smaller seen time
+	For Greedy, we pick higher stored value	*/
+bool node_comparator(node *a, node *b)
+{
+	node x = *a;
+	node y = *b;
 
-    if (x.value > y.value)  return true;
-    else return false;
+	bool ret = false;
+
+	if (strcmp(flag, "dfs") == 0) {
+		if (x.seen_time > y.seen_time)  ret = true;
+	}
+	else if (strcmp(flag, "bfs") == 0) {
+		if (x.seen_time < y.seen_time)  ret = true;
+	}
+	else if (strcmp(flag, "greedy") == 0) {
+		if (x.value > y.value)  ret = true;
+	}
+
+	return ret;
 }
 
-void swap(node *a, node *b) {
-    node *c = (node *)malloc(sizeof(node));
-    memcpy(c, a, sizeof(node));
-    memcpy(a, b, sizeof(node));
-    memcpy(b, c, sizeof(node));
+void swap(node *a, node *b)
+{
+	node *c = (node *)malloc(sizeof(node));
+	memcpy(c, a, sizeof(node));
+	memcpy(a, b, sizeof(node));
+	memcpy(b, c, sizeof(node));
+	free(c);
 }
 
-node *init_node(int index) {
-    node *ptr = (node *)malloc(sizeof(node));
-    ptr->state_number = index;
-    ptr->value = -1;
-    ptr->parent = -1;
-    ptr->number_of_children = 0;
-    for (int i = 0; i < MAX_CHILDREN; i++) {
-        ptr->children[i] = NULL;
-    }
-    return ptr;
+node *init_node(int index)
+{
+	node *ptr = (node *)malloc(sizeof(node));
+
+	ptr->state_number = index;
+	ptr->value = -1;
+	ptr->parent = -1;
+	ptr->number_of_children = 0;
+	for (int i = 0; i < MAX_CHILDREN; i++) {
+		ptr->children[i] = NULL;
+	}
+	ptr->seen_time = 0;
+	ptr->depth = 0;
+
+	return ptr;
 }
--- node/node.h
+++ node/node.h
@@ -6,24 +6,24 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define MAX_CHILDREN 1000
+#define MAX_CHILDREN 50
 
 typedef struct node node;
 
 struct node {
-    int state_number;
-    int value;
-    int parent;
-    int number_of_children;
-    node *children[MAX_CHILDREN];
-
-    // You (as a developer) can add any extra information here [Will not be touched by user]
-    // int depth;
-    // int seen_time;
+	int state_number;
+	int value;
+	int parent;
+	int number_of_children;
+	node *children[MAX_CHILDREN];
+	int seen_time;
+	int depth;
 };
 
-bool node_comparator(node *a, node *b);
-void swap(node *a, node *b);
-node *init_node(int index);
+char *flag;		// A global flag variable that takes in the argv[1] from the terminal
+bool node_comparator(node *a, node *b);		// Comparator function
+
+void swap(node *a, node *b);	// Swaps two nodes
+node *init_node(int index);		// Initializes a node
 
 #endif
