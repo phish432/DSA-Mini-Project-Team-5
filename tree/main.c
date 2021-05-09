c04ebffe432c603fc0a67860ef8586b80d74bc05
Keshav <agarwalkeshav4400@gmail.com>
2021-05-09 19:36:19 +0530

    corrected main.c for tree

--- tree/main.c
+++ tree/main.c
@@ -4,13 +4,10 @@ int main(void) {
     int size;
     scanf("%d", &size);
 
-    node *input[size];
+    node input[size];
+    
     for (int i = 0; i < size; i++) {
-        input[i] = (node *)malloc(sizeof(node));
-    }
-
-    for (int i = 0; i < size; i++) {
-        scanf("%d%d%d", &(input[i]->state_number), &(input[i]->value), &(input[i]->parent));
+        scanf("%d%d%d", &(input[i].state_number), &(input[i].value), &(input[i].parent));
     }
 
     tree *T = init_tree();
